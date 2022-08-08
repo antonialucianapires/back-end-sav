386475e36671ba14f9dfb8bfeed5a57e10e44045
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-08-07 23:30:47 -0300

    config/fix: ajuste no tratamento de excecao + cors

--- src/main/java/br/com/escola/sav/dto/request/periodo/PeriodoRequestDTO.java
+++ src/main/java/br/com/escola/sav/dto/request/periodo/PeriodoRequestDTO.java
@@ -6,6 +6,8 @@ import br.com.escola.sav.util.validador.UniqueValue;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -17,12 +19,16 @@ public class PeriodoRequestDTO implements Serializable {
 
     private static final long serialVersionUID = 1972131416413407202L;
 
+    @NotBlank(message = "nome do período não pode ser vazio")
     @UniqueValue(domainClass = Periodo.class, fieldName = "nome", message = "Já existe um período com este nome")
     private String nomePeriodo;
+    @NotBlank(message = "tipo do período não pode ser vazio")
     private int tipoPeriodo;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @NotNull(message = "data de início não pode ser vazia")
     private Date dataInicio;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @NotNull(message = "data fim não pode ser vazia")
     private Date dataFim;
     private List<SubperiodoRequestDTO> subperiodos;
 
