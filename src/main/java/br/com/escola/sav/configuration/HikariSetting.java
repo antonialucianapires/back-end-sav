a9a3041ce1bfe2a501391563e2302f7abf8c9835
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-08-15 20:50:27 -0300

    fix: redução do wait_timeout

---
+++ src/main/java/br/com/escola/sav/configuration/HikariSetting.java
@@ -0,0 +1,18 @@
+package br.com.escola.sav.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HikariSetting{
+
+    @Bean
+    public HikariConfig config() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.addDataSourceProperty("socketTimeout", 600000);
+        hikariConfig.setMaxLifetime(600000);
+        return hikariConfig;
+    }
+
+}
